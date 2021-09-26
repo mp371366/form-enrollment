e7b9eecb2a0f67e40bc98acbb48c5421e8e3549f
mp371366 <mp371366@github.com>
2021-09-26 19:43:34 +0200

    Add theme changing.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,11 +9,36 @@ import Cookies from '../Cookies/Cookies';
 import Home from '../Home/Home';
 import NoMatch from '../NoMatch/NoMatch';
 import ReportForm from '../ReportForm/ReportForm';
+import { CSSProperties, useState } from 'react';
 
 export default function App() {
+  const [theme, setTheme] = useState<'dark' | 'light'>('dark');
+  function changeTheme() {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  }
+
+  const themeStyle: CSSProperties
+    = theme === 'dark'
+    ? {
+      "--main-bg-color": "#282c34",
+      "--snd-bg-color": "rgb(71, 71, 71)",
+      "--font-color": "white",
+    } as CSSProperties
+    : {
+      "--main-bg-color": "white",
+      "--snd-bg-color": "rgb(222, 222, 222)",
+      "--font-color": "black",
+    } as CSSProperties
+  ;
+  const icon
+    = theme === 'dark'
+    ? 'fa fa-sun-o'
+    : 'fa fa-moon-o'
+  ;
+
   return (
     <Router>
-      <div className="App">
+      <div className="App" style={themeStyle}>
         <nav className="App-header">
           <ul>
             <li>
@@ -23,6 +48,9 @@ export default function App() {
               <Link className="link" to="/cookie">Cookies policy</Link>
             </li>
           </ul>
+          <button className="App-theme-button" onClick={changeTheme}>
+            <i className={icon} aria-hidden="true" /> 
+          </button>
         </nav>
         <main className="App-main">
           <Switch>
